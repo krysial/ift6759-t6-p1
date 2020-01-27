fc34190951204620c509b5de06e0d27c6dff9b7f
mahmoud ™ <mahmoud.abounassif@gmail.com>
2020-01-27 12:45:22 -0500

    Remove useless files (#6)

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module load python/3.7
-virtualenv --no-download ~/ift6759-env
-source ~/ift6759-env/bin/activate
-pip install --no-index --upgrade pip
-pip install --no-index --upgrade tensorflow_gpu
-pip install -r requirements.txt
