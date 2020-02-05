d7469728165eebf6c9c0f71e97f80f34d2bc239d
krysial <54903848+krysial@users.noreply.github.com>
2020-02-05 17:34:56 -0500

    Get raw images (#14)
    * Get raw images
    
    * [UPDATE] Opening h5 files only once
    
    * Get raw images: Adding check for None frames

--- dataloader/get_raw_images.py
+++ dataloader/get_raw_images.py
@@ -1,22 +1,128 @@
+import typing
+import datetime as dt
+import json
+
+import pandas as pd
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+from utils import utils
+
+IMAGE_HEIGHT = 650
+IMAGE_WIDTH = 1500
+DT_FORMAT = '%Y-%m-%dT%H:%M:%S'
+GOES13_DS = {
+    'hdf516': ['hdf5_16bit_path', 'hdf5_16bit_offset'],
+    'hdf508': ['hdf5_8bit_path', 'hdf5_8bit_offset']
+}
 
 
 def get_raw_images(
         dataframe: pd.DataFrame,
-        batch_of_datetimes: typing.List[datetime.datetime],
+        datetimes: typing.List[dt.datetime],
         config: typing.Dict[typing.AnyStr, typing.Any],) -> np.ndarray:
     """
-    A function to get the raw input images as taken by the satelite
+    A function to get the raw input images as taken by the satellite
 
     Args:
         dataframe: a pandas dataframe that provides the netCDF file path (or HDF5 file path and offset).
-        batch_of_datetimes: a batch of timestamps that is required by the data loader to provide raw uncropped versions of
-                            the satelite images for the model.
+        datetimes: a batch of timestamps that is required by the data loader to provide raw uncropped versions of
+                            the satellite images for the model.
         config: configuration dictionary holding any extra parameters that might be required for tuning purposes.
 
     Returns:
-        A numpy array of shape (batch_size, temporal_seq, channels, length, bredth)
+        A numpy array of shape (batch_size, temporal_seq, channels, length, width)
     """
 
-    assert images.shape == (len(batch_of_datetimes),
-                            config['no_of_temporal_seq'], 5, L, B)
-    return images
+    channels = config['channels']
+    seqs = config['no_of_temporal_seq']
+    goes13_i_paths = get_frames_location(dataframe, datetimes, seqs, config['goes13_dataset'])
+    frames = fetch_frames(goes13_i_paths, channels, seqs)
+
+    assert frames.shape == (len(datetimes), seqs, len(channels), IMAGE_HEIGHT, IMAGE_WIDTH)
+    return frames
+
+# auxiliary functions
+
+
+def read_conf_file(path):
+    with open(path) as f:
+        config = json.load(f)
+    return config
+
+
+def get_frames_location(dataframe, datetimes, seqs, dataset):
+
+    columns = GOES13_DS[dataset] if dataset else 'hdf516'
+    offset = 15
+    dt_seqs = []
+
+    for i, datetime in enumerate(datetimes):
+        for j in range(seqs):
+            dt_seqs.append({
+                'datetime': datetime - dt.timedelta(minutes=(offset * j)),
+                'position': (i, j)
+            })
+
+    df = pd.DataFrame(dt_seqs)
+    df['path'] = dataframe.loc[df['datetime']][columns[0]].to_list()
+    df['offset'] = dataframe.loc[df['datetime']][columns[1]].to_list()
+
+    return df
+
+
+def fetch_frames(frames_df, channels, seqs):
+    output = np.empty((frames_df.shape[0] // seqs, seqs, len(channels), IMAGE_HEIGHT, IMAGE_WIDTH))
+
+    paths_groups = frames_df.groupby('path', sort=False)
+
+    for name, group in paths_groups:
+        with h5py.File(name, "r") as h5_data:
+            for index, row in group.iterrows():
+                position = row['position']
+                frame = row['offset']
+                for c, channel in enumerate(channels):
+                    channel_idx_data = utils.fetch_hdf5_sample(channel, h5_data, frame)
+                    if channel_idx_data is None or channel_idx_data.shape != (IMAGE_HEIGHT, IMAGE_WIDTH):
+                        channel_idx_data = 0
+                    output[position[0], position[1], c] = channel_idx_data
+
+    return output
+
+
+def show_frame(image):
+    assert image.shape == (IMAGE_HEIGHT, IMAGE_WIDTH), 'Unexpected frame shape'
+    fig, ax = plt.subplots(1)
+    ax.imshow(image, cmap='bone')
+    plt.show()
+
+
+def _main():
+    # expected usage
+    app_config = {
+        "no_of_temporal_seq": 5,
+        "dataframe_path": "/project/cq-training-1/project1/data/dummy_test_catalog.pkl",
+        "channels": ["ch1", "ch2", "ch3", "ch4", "ch6"],
+        "goes13_dataset": 'hdf516',
+        "target_datetimes": [
+            "2015-01-01T12:45:00",
+            "2015-01-01T19:00:00",
+            "2015-01-02T13:30:00",
+            "2015-01-02T19:45:00"
+        ]
+    }
+    dataframe_path = app_config['dataframe_path']
+    catalog = pd.read_pickle(dataframe_path)
+    batch_of_datetimes = [dt.datetime.strptime(i, DT_FORMAT) for i in app_config['target_datetimes']]
+
+    raw_images = get_raw_images(catalog, batch_of_datetimes, app_config)
+
+    for i in range(app_config["no_of_temporal_seq"]):
+        show_frame(raw_images[1, i, 0])
+
+    print('Done!')
+
+
+if __name__ == "__main__":
+    _main()
