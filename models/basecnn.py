69c9edc087cd249b5911f083ad8025e776cab148
ankur <ankuronetwo@gmail.com>
2020-02-20 04:57:07 +0000

    moved reshaping inside model

--- models/basecnn.py
+++ models/basecnn.py
@@ -6,6 +6,17 @@ from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 
 
+def stack_seq(x):
+    x_t = tf.transpose(x, [0, 2, 3, 1, 4])
+    sh = x_t.shape
+    x_r = tf.reshape(x_t, shape=[-1, sh[1], sh[2], sh[3] * sh[4]])
+    return x_r
+
+
+def stack_seq_shape(input_shape):
+    return tuple(-1, input_shape[2], input_shape[0] * input_shape[3])
+
+
 class BaseCNNModel(tf.keras.Model):
     @classmethod
     def create(
@@ -16,6 +27,7 @@ class BaseCNNModel(tf.keras.Model):
     ):
         self = cls()
         self.cnn_model = Sequential([
+            Lambda(stack_seq, output_shape=stack_seq_shape),
             Conv2D(64, 5, strides=1, activation="relu"),
             Conv2D(64, 5, strides=1, activation="relu"),
             MaxPool2D(pool_size=2, strides=2),
