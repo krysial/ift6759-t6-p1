fc6888fceb132917dbbccfc99f83ec3f696ad0a0
ankur <ankuronetwo@gmail.com>
2020-02-20 03:17:09 +0000

    import redundancies removed

--- models/basecnn.py
+++ models/basecnn.py
@@ -2,6 +2,9 @@ import typing
 import datetime
 import tensorflow as tf
 
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+
 
 class BaseCNNModel(tf.keras.Model):
     @classmethod
@@ -12,15 +15,15 @@ class BaseCNNModel(tf.keras.Model):
         config: typing.Dict[typing.AnyStr, typing.Any]
     ):
         self = cls()
-        self.cnn_model = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(64, 5, strides=1, activation=tf.nn.relu),
-            tf.keras.layers.Conv2D(64, 5, strides=1, activation=tf.nn.relu),
-            tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
-            tf.keras.layers.Conv2D(128, 3, strides=(1, 1), activation=tf.nn.relu),
-            tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(units=512, activation=tf.nn.relu),
-            tf.keras.layers.Dense(units=len(target_time_offsets))
+        self.cnn_model = Sequential([
+            Conv2D(64, 5, strides=1, activation="relu"),
+            Conv2D(64, 5, strides=1, activation="relu"),
+            MaxPool2D(pool_size=2, strides=2),
+            Conv2D(128, 3, strides=(1, 1), activation="relu"),
+            MaxPool2D(pool_size=2, strides=2),
+            Flatten(),
+            Dense(units=512, activation="relu"),
+            Dense(units=len(target_time_offsets))
         ])
 
         return self
