d5c78bfbf1cb721e67676a857dfeb5eaa36a6a09
ankur <ankuronetwo@gmail.com>
2020-02-20 21:24:42 +0000

    changed Sequential to Model

--- models/convlstm.py
+++ models/convlstm.py
@@ -22,55 +22,59 @@ class ConvLSTMModel(BaseModel):
     ):
         self = cls()
 
-        self.sequece = Sequential()
-        self.sequece.add(BatchNormalization())
-        self.sequece.add(ConvLSTM2D(filters=64,
-                                    kernel_size=(3, 3),
-                                    activation='relu',
-                                    padding='valid', return_sequences=True))
+        img_input_shape = (config['seq_len'], config['crop_size'], config['crop_size'], len(config['channels']))
+
+        i = Input(shape=img_input_shape)
+
+        m = BatchNormalization()(i)
+        m = ConvLSTM2D(filters=64,
+                       kernel_size=(3, 3),
+                       activation='relu',
+                       padding='valid', return_sequences=True)(m)
 
         for _ in range(config['seq_len'] - 4):
-            self.sequece.add(BatchNormalization())
-            self.sequece.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
-            self.sequece.add(Dropout(0.33))
-            self.sequece.add(ConvLSTM2D(filters=64 * 2**(_ // 2),
-                                        kernel_size=(3, 3),
-                                        activation='relu',
-                                        padding='same', return_sequences=True))
+            m = BatchNormalization()(m)
+            m = MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2))(m)
+            m = Dropout(0.33)(m)
+            m = ConvLSTM2D(filters=64 * 2**(_ // 2),
+                           kernel_size=(3, 3),
+                           activation='relu',
+                           padding='same', return_sequences=True)(m)
 
-        self.sequece.add(BatchNormalization())
-        self.sequece.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
-        self.sequece.add(Dropout(0.33))
-        self.sequece.add(ConvLSTM2D(filters=64 * 2**((_ + 1)),
-                                    kernel_size=(3, 3),
-                                    activation='relu',
-                                    padding='same', return_sequences=True))
+        m = BatchNormalization()(m)
+        m = MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2))(m)
+        m = Dropout(0.33)(m)
+        m = ConvLSTM2D(filters=64 * 2**((_ + 1)),
+                       kernel_size=(3, 3),
+                       activation='relu',
+                       padding='same', return_sequences=True)(m)
 
-        self.sequece.add(BatchNormalization())
-        self.sequece.add(MaxPooling3D(pool_size=(1, 3, 3), strides=(1, 2, 2)))
-        self.sequece.add(Dropout(0.33))
-        self.sequece.add(ConvLSTM2D(filters=64 * 2**((_ + 2)),
-                                    kernel_size=(3, 3),
-                                    activation='relu',
-                                    padding='same', return_sequences=True))
+        m = BatchNormalization()(m)
+        m = MaxPooling3D(pool_size=(1, 3, 3), strides=(1, 2, 2))(m)
+        m = Dropout(0.33)(m)
+        m = ConvLSTM2D(filters=64 * 2**((_ + 2)),
+                       kernel_size=(3, 3),
+                       activation='relu',
+                       padding='same', return_sequences=True)(m)
 
-        self.sequece.add(TimeDistributed(Flatten(), name='TD.Flatten'))
-        self.sequece.add(TimeDistributed(BatchNormalization(), name='TD.BN1'))
-        self.sequece.add(TimeDistributed(Dropout(0.2), name='TD.Dropout1'))
-        self.sequece.add(TimeDistributed(Dense(1024, activation='relu'), name='TD.Dense1'))
-        self.sequece.add(TimeDistributed(BatchNormalization(), name='TD.BN2'))
-        self.sequece.add(TimeDistributed(Dropout(0.2), name='TD.Dropout2'))
-        self.sequece.add(TimeDistributed(Dense(1024, activation='relu'), name='TD.Dense2'))
-        self.sequece.add(TimeDistributed(BatchNormalization(), name='TD.BN3'))
-        self.sequece.add(TimeDistributed(Dropout(0.2), name='TD.Dropout3'))
-        self.sequece.add(Flatten())
+        m = TimeDistributed(Flatten(), name='TD.Flatten')(m)
+        m = TimeDistributed(BatchNormalization(), name='TD.BN1')(m)
+        m = TimeDistributed(Dropout(0.2), name='TD.Dropout1')(m)
+        m = TimeDistributed(Dense(1024, activation='relu'), name='TD.Dense1')(m)
+        m = TimeDistributed(BatchNormalization(), name='TD.BN2')(m)
+        m = TimeDistributed(Dropout(0.2), name='TD.Dropout2')(m)
+        m = TimeDistributed(Dense(1024, activation='relu'), name='TD.Dense2')(m)
+        m = TimeDistributed(BatchNormalization(), name='TD.BN3')(m)
+        m = TimeDistributed(Dropout(0.2), name='TD.Dropout3')(m)
+        m = Flatten()(m)
 
-        self.sequece.add(Dense(4))
-        self.sequece.add(Dense(4))
-        self.sequece.add(Dense(4))
-        self.sequece.add(Dense(4))
+        m = Dense(4)(m)
+        m = Dense(4)(m)
+        m = Dense(4)(m)
+        o = Dense(4)(m)
 
+        self.model = Model(i, o)
         return self
 
     def call(self, inputs):
-        return self.sequece(inputs['images'])
+        return self.model(inputs['images'])
