54c901502c0702e4db8481abd994c280ae064b8f
mabounassif <mahmoud.abounassif@gmail.com>
2020-02-22 20:05:17 +0000

    Fix batch script to support array job

--- preprocessing_slurm.sh
+++ preprocessing_slurm.sh
@@ -5,6 +5,21 @@
 #SBATCH --mem=4G
 #SBATCH -o ./results/slurm-%A_%a.out # STDOUT
 
+start_indices=(0    200 400 600 800  1000 1200 1400 1600 1800 2000)
+end_indices=(  200  400 600 800 1000 1200 1400 1600 1800 2000)
+
 module load python/3.7
 source /project/cq-training-1/project1/teams/team06/ift6759-env/bin/activate
-python preprocessing.py --start-index=103
+
+if [ "${#end_indices[@]}" -gt "$(($SLURM_ARRAY_TASK_ID))" ]
+then
+    echo "start ${start_indices[$SLURM_ARRAY_TASK_ID]}"
+    echo "start ${end_indices[$SLURM_ARRAY_TASK_ID]}"
+    python preprocessing.py --start-index=${start_indices[$SLURM_ARRAY_TASK_ID]} --end-index=${end_indices[$SLURM_ARRAY_TASK_ID]}
+elif [ "${#end_indices[@]}" -eq "$(($SLURM_ARRAY_TASK_ID))" ]
+then
+    echo "start ${start_indices[$SLURM_ARRAY_TASK_ID]}"
+    python preprocessing.py --start-index=${start_indices[$SLURM_ARRAY_TASK_ID]}
+else
+    echo "OUT OF BOUNDS"
+fi
