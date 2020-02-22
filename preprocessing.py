91a2bb1be02536d38159d100b6dfad6df39bfd3f
mabounassif <mahmoud.abounassif@gmail.com>
2020-02-22 02:19:37 +0000

    Implement a generator to lower memory needs

--- preprocessing.py
+++ preprocessing.py
@@ -25,9 +25,10 @@ OUTPUT_PATH = "/project/cq-training-1/project1/teams/team06/cache/hdf5v5_16bit/"
 
 
 class Worker(Thread):
-    def __init__(self, queue, config):
+    def __init__(self, queue, config, save_path_base):
         Thread.__init__(self)
         self.queue = queue
+        self.save_path_base = save_path_base
         self.stations_px_center = []
         self.px_offset = config.crop_size // 2
         self.config = config
@@ -47,88 +48,92 @@ class Worker(Thread):
             self.queue.task_done()
 
     def worker_task(self, data):
+        c, path = data
+
+        output_file = self.save_path_base + \
+            os.path.basename(os.path.normpath(path))
+
+        fp = np.memmap(
+            output_file,
+            dtype='float32',
+            mode='w+',
+            shape=(
+                len(config.channels),
+                96,
+                len(self.stations),
+                self.config.crop_size,
+                self.config.crop_size
+            )
+        )
+
+        for i, line in enumerate(self.generator(data)):
+            fp[i] = line
+            fp.flush()
+
+    def generator(self, data):
         lats = None
         lons = None
         c, path = data
 
-        if os.path.isfile(path):
-            with h5py.File(path, "r") as h5_data:
-                file_data = []
-                start = time.time()
-                for channel in ['ch1', 'ch2', 'ch3', 'ch4', 'ch6']:
-                    ch_images = []
-                    for offset in range(96):
-                        channel_idx_data = utils.fetch_hdf5_sample(channel, h5_data, offset)
-                        if channel_idx_data is None or channel_idx_data.shape != (IMAGE_HEIGHT, IMAGE_WIDTH):
-                            ch_images.append(None)
-                        else:
-                            if not self.stations_px_center:
-                                i = 0
-                                while lats is None or lons is None:
-                                    lats = utils.fetch_hdf5_sample("lat", h5_data, i)
-                                    lons = utils.fetch_hdf5_sample("lon", h5_data, i)
-                                    i += 1
+        try:
+            if os.path.isfile(path):
+                with h5py.File(path, "r") as h5_data:
+                    file_data = []
+                    start = time.time()
+                    for channel in ['ch1', 'ch2', 'ch3', 'ch4', 'ch6']:
+                        ch_images = []
+                        for offset in range(96):
+                            channel_idx_data = utils.fetch_hdf5_sample(channel, h5_data, offset)
+                            if channel_idx_data is None or channel_idx_data.shape != (IMAGE_HEIGHT, IMAGE_WIDTH):
+                                ch_images.append([np.zeros((self.config.crop_size, self.config.crop_size))] * len(self.stations))
+                            else:
+                                if not self.stations_px_center:
+                                    i = 0
+                                    while lats is None or lons is None:
+                                        lats = utils.fetch_hdf5_sample("lat", h5_data, i)
+                                        lons = utils.fetch_hdf5_sample("lon", h5_data, i)
+                                        i += 1
 
-                                if lats is None or lons is None:
-                                    continue
+                                    if lats is None or lons is None:
+                                        continue
 
-                                def red_coords_to_px(aggre, s):
-                                    lat, lon, _ = self.stations[s]
-                                    px_lat = len(lats) * ((lat - lats[0]) / (lats[-1] - lats[0]))
-                                    px_lon = len(lons) * ((lon - lons[0]) / (lons[-1] - lons[0]))
+                                    def red_coords_to_px(aggre, s):
+                                        lat, lon, _ = self.stations[s]
+                                        px_lat = len(lats) * ((lat - lats[0]) / (lats[-1] - lats[0]))
+                                        px_lon = len(lons) * ((lon - lons[0]) / (lons[-1] - lons[0]))
 
-                                    del lat
-                                    del lon
+                                        del lat
+                                        del lon
+                                        gc.collect()
+
+                                        aggre[s] = (int(px_lat), int(px_lon))
+                                        return aggre
+
+                                    self.stations_px_center = functools.reduce(red_coords_to_px, self.stations, {})
+
+                                    del lats
+                                    del lons
                                     gc.collect()
 
-                                    aggre[s] = (int(px_lat), int(px_lon))
-                                    return aggre
+                                def crop(s):
+                                    center = self.stations_px_center[s]
+                                    px_x_ = center[0] - self.px_offset
+                                    px_x = center[0] + self.px_offset
+                                    px_y_ = center[1] - self.px_offset
+                                    px_y = center[1] + self.px_offset
+                                    gc.collect()
+                                    crop = channel_idx_data[px_x_:px_x, px_y_:px_y].copy()
+                                    return crop
 
-                                self.stations_px_center = functools.reduce(red_coords_to_px, self.stations, {})
+                                ch_images.append(list(map(crop, self.stations_px_center)))
 
-                                del lats
-                                del lons
+                                del channel_idx_data
+                                gc.collect()
 
-                            def crop(s):
-                                center = self.stations_px_center[s]
-                                px_x_ = center[0] - self.px_offset
-                                px_x = center[0] + self.px_offset
-                                px_y_ = center[1] - self.px_offset
-                                px_y = center[1] + self.px_offset
-                                crop = channel_idx_data[px_x_:px_x, px_y_:px_y].copy()
-                                return crop
-
-                            ch_images.append(list(map(crop, self.stations_px_center)))
-
-                            del channel_idx_data
-                            gc.collect()
-
-                    file_data.append(ch_images.copy())
-                    del ch_images
-                    gc.collect()
-
-                save_path_base = OUTPUT_PATH + str(self.config.crop_size) + '/'
-
-                if not(os.path.exists(save_path_base)):
-                    os.makedirs(save_path_base)
-
-                end = time.time()
-                print('One file processed', end - start)
-                result = np.asanyarray(file_data.copy())
-                del file_data
-                gc.collect()
-                np.save(
-                    save_path_base +
-                    os.path.basename(os.path.normpath(path)),
-                    result
-                )
-                end = time.time()
-                print('One file saved', end - start)
-
-                del result
-                h5_data.close()
-                del h5_data
-                gc.collect()
+                        yield np.asanyarray(ch_images.copy())
+        finally:
+            del ch_images
+            gc.collect()
 
 
 def main(config, dataframe):
@@ -140,8 +145,12 @@ def main(config, dataframe):
 
     print('Processing ', len(unique_paths), ' files')
 
+    save_path_base = OUTPUT_PATH + str(config.crop_size) + '/'
+    if not(os.path.exists(save_path_base)):
+        os.makedirs(save_path_base)
+
     for x in range(15):
-        worker = Worker(queue, config)
+        worker = Worker(queue, config, save_path_base)
         # Setting daemon to True will let the main thread exit even though the workers are blocking
         worker.daemon = True
         worker.start()
@@ -162,6 +171,7 @@ def main(config, dataframe):
 
 
 if __name__ == '__main__':
+    DEFAULT_CHANNELS = ["ch1", "ch2", "ch3", "ch4", "ch6"]
     dataframe = pd.read_pickle(DATAFRAME_PATH)
 
     parser = argparse.ArgumentParser()
@@ -176,6 +186,14 @@ if __name__ == '__main__':
         type=int,
         help="end dateframe index",
         default=len(dataframe)
+    )
+    parser.add_argument(
+        "--channels",
+        dest='channels',
+        help="channels to keep",
+        type=str,
+        nargs='*',
+        default=DEFAULT_CHANNELS
     )
     parser.add_argument(
         "--crop-size",
