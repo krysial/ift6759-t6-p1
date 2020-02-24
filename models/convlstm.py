b1feb840812e0d9d8818c7fa393909437fbe7246
ankur <ankuronetwo@gmail.com>
2020-02-24 04:13:30 +0000

    model updated for any size less than 80x80

--- models/convlstm.py
+++ models/convlstm.py
@@ -15,6 +15,23 @@ import numpy as np
 
 
 class ConvLSTMModel(BaseModel):
+    def ConvLSTM_block(self, model, filters, kernel_size, activation, padding, return_sequences, pool_size, pool_strides, drop_prob):
+        model = ConvLSTM2D(filters=filters,
+                           kernel_size=kernel_size,
+                           padding=padding,
+                           return_sequences=return_sequences)(model)
+        model = BatchNormalization()(model)
+        model = Activation(activation)(model)
+        model = MaxPooling3D(pool_size=pool_size, strides=pool_strides)(model)
+        model = Dropout(drop_prob)(model)
+        return model
+
+    def TD_Dense_block(self, model, drop_prob, units, activation, name):
+        model = TimeDistributed(Dense(units, activation=activation), name='TD.Dense' + name)(model)
+        model = TimeDistributed(BatchNormalization(), name='TD.BN' + name)(model)
+        model = TimeDistributed(Dropout(drop_prob), name='TD.Dropout' + name)(model)
+        return model
+
     @classmethod
     def create(
         cls,
@@ -23,60 +40,90 @@ class ConvLSTMModel(BaseModel):
         config: typing.Dict[typing.AnyStr, typing.Any]
     ):
         self = cls()
+        activation = 'relu'
 
-        img_input_shape = (config['seq_len'], config['crop_size'], config['crop_size'], len(config['channels']))
+        def img_model():
+            img_input_shape = (config['seq_len'], config['crop_size'], config['crop_size'], len(config['channels']))
+            img = Input(shape=img_input_shape)
+            m = ZeroPadding3D(padding=(0, (80 - config['crop_size']) // 2, (80 - config['crop_size']) // 2), data_format=None)(img)
+            m = TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2)))(m)
+            m = self.ConvLSTM_block(
+                model=m,
+                filters=32,
+                kernel_size=(5, 5),
+                activation=activation,
+                padding='same',
+                return_sequences=True,
+                pool_size=(1, 2, 2),
+                pool_strides=(1, 2, 2),
+                drop_prob=0.33,
+            )
+            m = self.ConvLSTM_block(
+                model=m,
+                filters=64,
+                kernel_size=(3, 3),
+                activation=activation,
+                padding='valid',
+                return_sequences=True,
+                pool_size=(1, 3, 3),
+                pool_strides=(1, 2, 2),
+                drop_prob=0.33,
+            )
+            m = TimeDistributed(Flatten(), name='TD.Flatten')(m)
+            return img, m
 
-        i = Input(shape=img_input_shape)
+        def clearsky_model():
+            if config['real']:
+                clearsky_input_shape = (config['seq_len'])
+            else:
+                clearsky_input_shape = (len(target_time_offsets))
+            clearsky = Input(shape=clearsky_input_shape)
+            if config['real']:
+                m = Reshape((config['seq_len'], 1))(clearsky)
+            else:
+                m = RepeatVector(config['seq_len'])(clearsky)
+            m = Dense(4096, activation=activation)(m)
+            return clearsky, m
 
-        m = BatchNormalization()(i)
-        m = ConvLSTM2D(filters=64,
-                       kernel_size=(3, 3),
-                       activation='relu',
-                       padding='valid', return_sequences=True)(m)
+        img_in, img_out = img_model()
+        clearsky_in, clearsky_out = clearsky_model()
 
-        for _ in range(config['seq_len'] - 4):
-            m = BatchNormalization()(m)
-            m = MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2))(m)
-            m = Dropout(0.33)(m)
-            m = ConvLSTM2D(filters=64 * 2**(_ // 2),
-                           kernel_size=(3, 3),
-                           activation='relu',
-                           padding='same', return_sequences=True)(m)
+        m = Add()([img_out, clearsky_out])
 
-        m = BatchNormalization()(m)
-        m = MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2))(m)
-        m = Dropout(0.33)(m)
-        m = ConvLSTM2D(filters=64 * 2**((_ + 1)),
-                       kernel_size=(3, 3),
-                       activation='relu',
-                       padding='same', return_sequences=True)(m)
+        m = self.TD_Dense_block(
+            model=m,
+            drop_prob=0.2,
+            units=1024,
+            activation=activation,
+            name='1'
+        )
 
-        m = BatchNormalization()(m)
-        m = MaxPooling3D(pool_size=(1, 3, 3), strides=(1, 2, 2))(m)
-        m = Dropout(0.33)(m)
-        m = ConvLSTM2D(filters=64 * 2**((_ + 2)),
-                       kernel_size=(3, 3),
-                       activation='relu',
-                       padding='same', return_sequences=True)(m)
+        m = self.TD_Dense_block(
+            model=m,
+            drop_prob=0.2,
+            units=512,
+            activation=activation,
+            name='2'
+        )
 
-        m = TimeDistributed(Flatten(), name='TD.Flatten')(m)
-        m = TimeDistributed(BatchNormalization(), name='TD.BN1')(m)
-        m = TimeDistributed(Dropout(0.2), name='TD.Dropout1')(m)
-        m = TimeDistributed(Dense(1024, activation='relu'), name='TD.Dense1')(m)
-        m = TimeDistributed(BatchNormalization(), name='TD.BN2')(m)
-        m = TimeDistributed(Dropout(0.2), name='TD.Dropout2')(m)
-        m = TimeDistributed(Dense(1024, activation='relu'), name='TD.Dense2')(m)
-        m = TimeDistributed(BatchNormalization(), name='TD.BN3')(m)
-        m = TimeDistributed(Dropout(0.2), name='TD.Dropout3')(m)
+        m = self.TD_Dense_block(
+            model=m,
+            drop_prob=0.2,
+            units=128,
+            activation=activation,
+            name='3'
+        )
+
+        m = LSTM(64, return_sequences=True)(m)
+        m = LSTM(64, return_sequences=True)(m)
+
         m = Flatten()(m)
+        m = Dense(len(target_time_offsets))(m)
+        m = Dense(len(target_time_offsets))(m)
+        o = Dense(len(target_time_offsets))(m)
 
-        m = Dense(4)(m)
-        m = Dense(4)(m)
-        m = Dense(4)(m)
-        o = Dense(4)(m)
-
-        self.model = Model(i, o)
+        self.model = Model([img_in, clearsky_in], o)
         return self
 
     def call(self, inputs):
-        return self.model(inputs['images'])
+        return self.model([inputs['images'], inputs['clearsky']])
--- train_on_slurm.sh
+++ train_on_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --gres=gpu:k80:1
+#SBATCH --gres=gpu:k20:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=3500M
 #SBATCH -o ./results/slurm-%A_%a.out # STDOUT
