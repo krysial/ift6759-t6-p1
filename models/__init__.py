c89e9e6176ef41f57566ac271a9d48c0cefd30f7
krysial <krysiallull@gmail.com>
2020-02-27 17:22:10 -0500

    Fix for scaled metric

--- models/__init__.py
+++ models/__init__.py
@@ -24,6 +24,31 @@ models = {
     "se_res_bilrcn": SE_Residual_BiLRCNModel,
 }
 
+# metrics
+def scaled_rmse(y_true, y_pred):
+    y_true_ = (y_true * 470.059048) + 297.487143
+    y_pred_ = (y_pred * 470.059048) + 297.487143
+
+    mse = tf.math.reduce_mean(tf.math.square(y_true_ - y_pred_))
+    rmse_loss = tf.math.sqrt(mse)
+    return rmse_loss
+
+
+# Root Relative Squared Error
+def rse(y_true, y_pred):
+    num = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(y_true - y_pred)))
+    den = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(y_pred - tf.math.reduce_mean(y_true, axis=None))))
+    return tf.math.reduce_mean(num / den)
+
+
+# Empirical Correlation Coefficient
+def CORR(y_true, y_pred):
+    num = tf.math.reduce_sum(
+        tf.math.multiply((y_true - tf.reduce_mean(y_true, axis=0)), (y_pred - tf.reduce_mean(y_pred, axis=0))))
+    den = tf.math.reduce_sum(tf.math.multiply(tf.math.square(y_true - tf.reduce_mean(y_true, axis=0)),
+                                              tf.math.square(y_pred - tf.reduce_mean(y_pred, axis=0))))
+    return tf.math.reduce_mean(num / den)
+
 
 def prepare_model_eval(
         stations: typing.Dict[typing.AnyStr, typing.Tuple[float, float, float]],
@@ -32,7 +57,7 @@ def prepare_model_eval(
 ) -> tf.keras.Model:
     model_path = os.path.join(config['checkpoint_path'], config['model_id'] + ".tf/")
     assert os.path.exists(model_path), f"No model found in path:{model_path}"
-    model = tf.keras.models.load_model(model_path, custom_objects={'scaled_rmse': scaled_rmse})
+    model = tf.keras.models.load_model(model_path, custom_objects={'scaled_rmse': scaled_rmse}, compile=False)
 
     return model
 
@@ -55,27 +80,6 @@ def prepare_model(
             target_time_offsets,
             config
         )
-
-        def scaled_rmse(y_true, y_pred):
-
-            y_true_ = (y_true * 470.059048) + 297.487143
-            y_pred_ = (y_pred * 470.059048) + 297.487143
-
-            mse = tf.math.reduce_mean(tf.math.square(y_true_ - y_pred_))
-            rmse_loss = tf.math.sqrt(mse)
-            return rmse_loss
-
-        # Root Relative Squared Error
-        def rse(y_true, y_pred):
-            num = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(y_true - y_pred)))
-            den = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(y_pred - tf.math.reduce_mean(y_true, axis=None))))
-            return tf.math.reduce_mean(num / den)
-
-        # Empirical Correlation Coefficient
-        def CORR(y_true, y_pred):
-            num = tf.math.reduce_sum(tf.math.multiply((y_true - tf.reduce_mean(y_true, axis=0)), (y_pred - tf.reduce_mean(y_pred, axis=0))))
-            den = tf.math.reduce_sum(tf.math.multiply(tf.math.square(y_true - tf.reduce_mean(y_true, axis=0)), tf.math.square(y_pred - tf.reduce_mean(y_pred, axis=0))))
-            return tf.math.reduce_mean(num / den)
 
         optimizer = Adadelta(lr=config['learning_rate'])
         model.compile(
